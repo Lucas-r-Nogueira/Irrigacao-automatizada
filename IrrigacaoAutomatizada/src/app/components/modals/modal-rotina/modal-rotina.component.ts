0870b4bf5d42981e7cebc0ebdcb6721d9983545a
Lucas Nogueira <107207138+Lucas-r-Nogueira@users.noreply.github.com>
2024-11-26 05:33:28 +0000

    Formato inválido dia semana rotina

--- IrrigacaoAutomatizada/src/app/components/modals/modal-rotina/modal-rotina.component.ts
+++ IrrigacaoAutomatizada/src/app/components/modals/modal-rotina/modal-rotina.component.ts
@@ -9,11 +9,12 @@ import { ModalController } from '@ionic/angular';  // Importando o ModalControll
   templateUrl: './modal-rotina.component.html',
   styleUrls: ['./modal-rotina.component.scss'],
 })
-export class ModalRotinaComponent  implements OnInit {
+export class ModalRotinaComponent implements OnInit {
   formRotina: FormGroup;
-  @Input() sensorId! : number;
+  @Input() sensorId!: number;
   @Output() rotinaCriada = new EventEmitter<void>(); // Emite um evento quando o sensor for criado com sucesso
-  
+  selectedOptions: string[] = []; // Armazena as opções selecionadas
+
   constructor(
     private formBuilder: FormBuilder,
     private rotinaService: RotinaService,
@@ -26,16 +27,15 @@ export class ModalRotinaComponent  implements OnInit {
     // Validações
     this.formRotina = this.formBuilder.group({
       id_sensor: [this.sensorId, Validators.required],
-      nome: [null, Validators.required], 
-      diaInicio: [null, Validators.required],
-      diaFim: [null, Validators.required],
-      horario_de_inicio: [null, Validators.required], 
+      nome: [null, Validators.required],
+      dias_de_ativacao: [[]],
+      horario_de_inicio: [null, Validators.required],
       horario_de_termino: [null, Validators.required],
     })
   }
 
   // Método para enviar os dados
-  onSubmit(){ 
+  onSubmit() {
     console.log(this.formRotina.value);
     if (this.formRotina.invalid) {
       console.log("Formulário Inválido");
@@ -45,7 +45,7 @@ export class ModalRotinaComponent  implements OnInit {
     const formValue = this.formRotina.value;
     let horarioDeInicio = new Date(formValue.horario_de_inicio);
     let horarioDeTermino = new Date(formValue.horario_de_termino);
-    
+
     // Verificar se os horários são válidos
     if (isNaN(horarioDeInicio.getTime()) || isNaN(horarioDeTermino.getTime())) {
       console.error("Horários inválidos");
@@ -55,6 +55,24 @@ export class ModalRotinaComponent  implements OnInit {
     // Converter os horários para o formato H:i:s
     formValue.horario_de_inicio = horarioDeInicio.toLocaleTimeString('pt-BR', { hour12: false });
     formValue.horario_de_termino = horarioDeTermino.toLocaleTimeString('pt-BR', { hour12: false });
+
+ 
+  // Inicializa a string de dias com underscores
+  let diasDeAtivacao = ['_', '_', '_', '_', '_', '_', '_']; // Um array de 7 caracteres ('_' para todos os dias inicialmente)
+
+  // Mapeia os dias selecionados para a string de dias
+  const diasSemana = ['M', 'T', 'W', 'T', 'F', 'S', 'S']; // Representação dos dias da semana em inglês
+  formValue.dias_de_ativacao.forEach((dia: string) => {
+    const diaIndex = parseInt(dia);  // Transformar o valor do dia em índice
+    if (diaIndex >= 0 && diaIndex <= 6) {
+      diasDeAtivacao[diaIndex] = diasSemana[diaIndex];  // Substitui o valor no índice correspondente
+    }
+  });
+
+  // Junta os caracteres do array para formar a string final
+  formValue.dias_de_ativacao = diasDeAtivacao.join('');
+
+  console.log("Dias de ativação:", formValue.dias_de_ativacao); // Exibe o resultado no formato 'MTWTF__'
 
     // Chama a service de criar o Rotina
     this.rotinaService.criarRotina(formValue).subscribe(
@@ -66,6 +84,7 @@ export class ModalRotinaComponent  implements OnInit {
       (error) => {
         console.error("Erro ao criar rotina:", error);
       }
-    );    
+    );
   }
+
 }
--- IrrigacaoAutomatizada/src/app/core/interface/Rotina.ts
+++ IrrigacaoAutomatizada/src/app/core/interface/Rotina.ts
@@ -2,9 +2,7 @@ export interface Rotinas {
     id?: number,
     id_sensor: number,
     nome: string,
-    dias_de_ativacao: string,
-    diaInicio: string,
-    diaFim: string,
+    dias_de_ativacao: [],
     horario_de_inicio: string,
     horario_de_termino: string,
 }

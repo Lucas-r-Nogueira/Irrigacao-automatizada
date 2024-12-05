2fe8415e46e77342aaa29c49f0875e922c5e8a56
Lucas Nogueira <107207138+Lucas-r-Nogueira@users.noreply.github.com>
2024-12-05 17:30:49 +0000

    Format created_at

--- IrrigacaoAutomatizada/src/app/components/card-historico/card-historico.component.ts
+++ IrrigacaoAutomatizada/src/app/components/card-historico/card-historico.component.ts
@@ -1,14 +1,23 @@
-import { Component, OnInit, Input} from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-card-historico',
   templateUrl: './card-historico.component.html',
   styleUrls: ['./card-historico.component.scss'],
 })
-export class CardHistoricoComponent  implements OnInit {
+export class CardHistoricoComponent implements OnInit {
   @Input() data: any; // Dados recebidos do pai  
-  constructor() { }
+
+  constructor() {}
 
   ngOnInit(): void {}
 
+  // Função para formatar a data no formato brasileiro (DD-MM-AAAA)
+  formatDate(dateString: string): string {
+    const date = new Date(dateString);
+    const day = String(date.getDate()).padStart(2, '0');
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const year = date.getFullYear();
+    return `${day}-${month}-${year}`;
+  }
 }
